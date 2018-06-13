2042a7c0bc8e109cf53e1994e64fd89e50b26c6f
Marsha331 <marsha331@gmail.com>
2018-06-13 22:27:27 +0000

    Submit Button works

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,9 +1,32 @@
 class CarsController < ApplicationController
   def index
     @cars = Car.all
+    p "*" * 100
+    p "index"
+    p params
+    p "*" * 100
   end
 
   def new
     @car = Car.new
+    p "*" * 100
+    p "new"
+    p params
+    p "*" * 100
+  end
+
+  def create
+    p "*" * 100
+    p "create"
+    p params
+    p "*" * 100
+    Car.create(car_params)
+    redirect_to cars_path
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:make, :model, :year, :color)
   end
 end
